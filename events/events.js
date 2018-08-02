cecb9652398c482d43121b1039078645475c0909
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 18:29:23 +0300

    yay

--- events/events.js
+++ events/events.js
@@ -1,12 +1,20 @@
-// Write all your JavaScript and JQuery code in this file! :)
-/* START PROVIDED CODE */
+
+
+
 function randomX() {
   return Math.random() * $(window).width();
 }
 
 function randomY() {
   return Math.random() * $(window).height();
+
 }
+$("#moveButton").on("click", function () {
+    console.log("Entered");
+
+    $("#message").css("left", randomX());
+    $("#message").css("top", randomY());
+});
 /* END PROVIDED CODE */
 
 // Write your code below this line :)
